6753b24eda857db1486364da26eafcd81c2c558b
Alok Singh <159884599+AlokSingh100@users.noreply.github.com>
2024-09-10 05:48:25 -0700

    Update matrix.java

--- pratic_java/matrix.java
+++ pratic_java/matrix.java
@@ -1,10 +1,14 @@
-package pootstap;
+package nevac;
 
-public class matrix {
+public class Nacit {
+
+
+
     public static void main(String[] args) {
         int[][] array = {{1,2,3},{4,2,5},{5,3,7}};
         int[][] array1 = {{5,6,7},{5,7,3},{9,5,2}};
         int[][] array4 = new int[array.length][array.length];
+
 
 
         for (int i = 0; i < array.length; i++) {
@@ -24,16 +28,27 @@ public class matrix {
 
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array.length; j++) {
-                array4[i][j]=array[i][j]+array1[i][j]*array[j][i]+array1[j][i]*array[i][i]+array1[j][j];
-            }
+                array4[i][j]=0;
+                 for (int k = 0; k < array.length; k++) {
 
+
+                     array4[i][j] += array[i][k] * array1[k][j];
+
+                 }
+
+            }
         }
+            
         System.out.println();
-        for (int i = 0; i < array.length; i++) {
+
+        for (int i = 0; i <array.length ; i++) {
             for (int j = 0; j < array.length; j++) {
                 System.out.print(array4[i][j]+" ");
             }
             System.out.println();
         }
+
+
+
     }
-}
+    }
