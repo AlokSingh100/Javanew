69c1cb774a944e9efbd9a62fdc5cc577aa2476cc
Alok Singh <159884599+AlokSingh100@users.noreply.github.com>
2024-09-23 18:53:39 +0530

    Add files via upload

---
+++ newpack/ecatiption.java
@@ -0,0 +1,17 @@
+package string;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class ecatiption extends Throwable {
+    public static void main(String[] args) {
+         Scanner scanner=new Scanner(System.in);
+        try {
+
+            int a=scanner.nextInt();
+        }catch (Exception e){
+        System.out.println(e);
+        }
+        System.out.println("hallo");
+    }
+}
