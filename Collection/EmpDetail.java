54e8901ce1d504648dd6c118d96b862cac4ea60b
Alok Singh <159884599+AlokSingh100@users.noreply.github.com>
2024-10-01 18:40:40 +0530

    Add files via upload

--- Collection/EmpDetail.java
+++ Collection/EmpDetail.java
@@ -1,4 +1,6 @@
-package scrip;
+package stapen;
+
+
 
 
 import java.util.ArrayList;
@@ -10,9 +12,9 @@ public class EmpDetail {
         List<Employee> emp=new ArrayList<Employee>();
         Scanner scanner=new Scanner(System.in);
         int select = 0, fet=0,inp=emp.size();
-        int idt=0,sid=0,i=0;
-        String name;
-        Float salary;
+        int idt=0,sid=0,ia=0,j=0,nit,fir,sip,dat = 0;
+        String name,sir ;
+        Float salary,tir ;
         while (true){
             System.out.println("Select Any one Option");
             System.out.println("1: Insert");
@@ -36,43 +38,58 @@ public class EmpDetail {
                     System.out.println("Employee ID  | Employee Name  | Employee Salary ");
                     for(Employee t:emp){
                         System.out.println(t);}
-                        break;
-                    
+                    break;
+
                 case 3:
                     System.out.println("Enter the Employee id ");
                     sid=scanner.nextInt();
                     System.out.println("Employee ID  | Employee Name  | Employee Salary ");
 
-                   for( i=0;i<=inp;i++) {
-                	   i++;
-                	   if (idt==sid) {
-                		   break;
-                	   }
-                   }
-                   System.out.println( emp.get(i));
-                   break;
+                    for(int i=0;i<=inp;i++) {
+
+                        if (idt==sid) {
+                            break;
+                        }
+                        ia++;
+                    }
+                    System.out.println( emp.get(ia+1));
+                    break;
                 case 4:
-//                	   System.out.println("Enter the Employee id ");
-//
-//                       for( i=0;i<=inp;i++) {
-//                    	   i++;
-//                    	   if (idt==sid) {
-//                    		   break;
-//                    	   }
-//                    	   emp.set(i, )
-                		
-                	}
-                	
+                	   System.out.println("Enter the Employee id ");
+                        nit=scanner.nextInt();
+                       for(int  i=0;i<=inp;i++) {
+
+                    	   if (idt==nit) {
+                    		   break;
+                    	   }
+                           j++;
+                       }
+                    System.out.println("Enter new employee id");
+                       fir=scanner.nextInt();
+                    System.out.println("Enter Employee Name");
+                    sir=scanner.next();
+                    System.out.println("Enter the Employee Salary");
+                    tir=scanner.nextFloat();
+                    emp.get(j).setEmpid(fir);
+                    emp.get(j).setEmpname(sir);
+                    emp.get(j).setSalary(tir);
+                case 5:
+                    System.out.println("Enter the Employee id ");
+                    sip=scanner.nextInt();
+                    for(int  i=0;i<=inp;i++) {
+
+                        if (idt==sip) {
+                            break;
+                        }
+                        dat++;
+                    }\xdcffrefdvfvf  fdvggrgtrgthtytgtrgtr \
             }
-
-
 
         }
 
 
-    }
-}
-
 
     }
+
+
 }
