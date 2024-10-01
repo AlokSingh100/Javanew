0f4384fdc775595273987056d195b97cd5889580
Alok Singh <159884599+AlokSingh100@users.noreply.github.com>
2024-10-01 19:28:17 +0530

    Update Employee.java

--- Collection/Employee.java
+++ Collection/Employee.java
@@ -1,43 +1,100 @@
-package Collection;
+package stapen;
 
-public class Employee {
 
-    int empid;
-    String empname;
-     float salary;
 
-    public Employee(int empid, String empname, float salary) {
-        this.empid = empid;
-        this.empname = empname;
-        this.salary = salary;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class EmpDetail {
+    public static void main(String[] args) {
+        List<Employee> emp=new ArrayList<Employee>();
+        Scanner scanner=new Scanner(System.in);
+        int select = 0, fet=0,inp=emp.size();
+        int idt=0,sid=0,ia=0,j=0,nit,fir,sip,dat = 0;
+        String name,sir ;
+        Float salary,tir ;
+        while (true){
+            System.out.println("Select Any one Option");
+            System.out.println("1: Insert");
+            System.out.println("2: Display");
+            System.out.println("3: Search");
+            System.out.println("4: Update");
+            System.out.println("5: Delete");
+
+            select=scanner.nextInt();
+            switch (select){
+                case 1:
+                    System.out.println("Enter the Employee Id");
+                    idt=scanner.nextInt();
+                    System.out.println("Enter Employee Name");
+                    name=scanner.next();
+                    System.out.println("Enter the Employee Salary");
+                    salary=scanner.nextFloat();
+                    emp.add(new Employee(idt,name,salary));
+                    break;
+                case 2:
+                    System.out.println("Employee ID  | Employee Name  | Employee Salary ");
+                    for(Employee t:emp){
+                        System.out.println(t);}
+                    break;
+
+                case 3:
+                    System.out.println("Enter the Employee id ");
+                    sid=scanner.nextInt();
+                    System.out.println("Employee ID  | Employee Name  | Employee Salary ");
+
+                    for(int i=0;i<=inp;i++) {
+
+                        if (idt==sid) {
+                            break;
+                        }
+                        ia++;
+                    }
+                    System.out.println( emp.get(ia));
+                    break;
+                case 4:
+                	   System.out.println("Enter the Employee id ");
+                        nit=scanner.nextInt();
+                       for(int  i=0;i<=inp;i++) {
+
+                    	   if (idt==nit) {
+                    		   break;
+                    	   }
+                           j++;
+                       }
+                    System.out.println("Enter new employee id");
+                       fir=scanner.nextInt();
+                    System.out.println("Enter Employee Name");
+                    sir=scanner.next();
+                    System.out.println("Enter the Employee Salary");
+                    tir=scanner.nextFloat();
+                    emp.get(j).setEmpid(fir);
+                    emp.get(j).setEmpname(sir);
+                    emp.get(j).setSalary(tir);
+                    break;
+                case 5:
+                    System.out.println("Enter the Employee id ");
+                    sip=scanner.nextInt();
+                    for(int  i=0;i<=inp;i++) {
+
+                        if (idt==sip) {
+                            break;
+                        }
+                        dat++;
+                    }
+                    emp.remove(dat);
+                    break;
+                default:
+                    System.out.println("Enter the right Input");
+            }
+
+        }
+
+
+
     }
 
-    public int getEmpid() {
-        return empid;
-    }
 
-    public void setEmpid(int empid) {
-        this.empid = empid;
-    }
-
-    public String getEmpname() {
-        return empname;
-    }
-
-    public void setEmpname(String empname) {
-        this.empname = empname;
-    }
-
-    public float getSalary() {
-        return salary;
-    }
-
-    public void setSalary(float salary) {
-        this.salary = salary;
-    }
-
-    @Override
-    public String toString() {
-        return "      "+empid+"           "+ empname+ "              "+salary ;
-    }
 }
