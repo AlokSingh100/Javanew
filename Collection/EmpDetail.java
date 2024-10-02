adce1709420d340bb502964202f17a920c016549
Alok Singh <159884599+AlokSingh100@users.noreply.github.com>
2024-10-02 19:11:44 +0530

    Update EmpDetail.java

--- Collection/EmpDetail.java
+++ Collection/EmpDetail.java
@@ -1,4 +1,4 @@
-package stapen;
+package Collection;
 
 
 
@@ -11,7 +11,7 @@ public class EmpDetail {
     public static void main(String[] args) {
         List<Employee> emp=new ArrayList<Employee>();
         Scanner scanner=new Scanner(System.in);
-        int select = 0, fet=0,inp=emp.size();
+        int select = 0, fet=0;
         int idt=0,sid=0,ia=0,j=0,nit,fir,sip,dat = 0;
         String name,sir ;
         Float salary,tir ;
@@ -21,7 +21,7 @@ public class EmpDetail {
             System.out.println("2: Display");
             System.out.println("3: Search");
             System.out.println("4: Update");
-            System.out.println("4: Deleate");
+            System.out.println("5: Delete");
 
             select=scanner.nextInt();
             switch (select){
@@ -45,44 +45,53 @@ public class EmpDetail {
                     sid=scanner.nextInt();
                     System.out.println("Employee ID  | Employee Name  | Employee Salary ");
 
-                    for(int i=0;i<=inp;i++) {
+                    for(int i=0;i<= emp.size();i++) {
 
-                        if (idt==sid) {
+//                        System.out.println("sid = "+ sid);
+//                        System.out.println("idt ="+emp.get(i).getEmpid() );
+                        if (sid==emp.get(i).getEmpid()) {
+                            System.out.println( emp.get(i));
                             break;
                         }
-                        ia++;
+//                        System.out.println(i);
                     }
-                    System.out.println( emp.get(ia+1));
                     break;
                 case 4:
                 	   System.out.println("Enter the Employee id ");
                         nit=scanner.nextInt();
-                       for(int  i=0;i<=inp;i++) {
+                       for(int  i=0;i<=emp.size();i++) {
 
-                    	   if (idt==nit) {
-                    		   break;
-                    	   }
-                           j++;
-                       }
-                    System.out.println("Enter new employee id");
+                    	   if (idt==emp.get(i).getEmpid()) {
+                               System.out.println("Enter new employee id");
                        fir=scanner.nextInt();
                     System.out.println("Enter Employee Name");
                     sir=scanner.next();
                     System.out.println("Enter the Employee Salary");
                     tir=scanner.nextFloat();
-                    emp.get(j).setEmpid(fir);
-                    emp.get(j).setEmpname(sir);
-                    emp.get(j).setSalary(tir);
+                    emp.get(i).setEmpid(fir);
+                    emp.get(i).setEmpname(sir);
+                    emp.get(i).setSalary(tir);
+                    		   break;
+                    	   }
+                           j++;
+                       }
+
+                    break;
                 case 5:
                     System.out.println("Enter the Employee id ");
                     sip=scanner.nextInt();
-                    for(int  i=0;i<=inp;i++) {
+                    for(int  i=0;i<=emp.size();i++) {
 
-                        if (idt==sip) {
+                        if (idt==emp.get(i).getEmpid()) {
+                            emp.remove(i);
                             break;
                         }
-                        dat++;
+
                     }
+                    break;
+                default:
+                    System.out.println("Enter the right Input");
+                    break;
             }
 
         }
