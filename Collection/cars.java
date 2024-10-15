8e296d00d97736922224bf41d5147f1e63fae74d
Alok Singh <159884599+AlokSingh100@users.noreply.github.com>
2024-10-15 18:33:56 +0530

    Add files via upload

--- Collection/cars.java
+++ Collection/cars.java
@@ -1,57 +1,60 @@
-package Collection;
+package Basic;
 
 public class cars {
-    String name;
-    int average;
-    int Miles;
-    double price;
 
-    public cars(String name, int average, int miles, double price) {
-        this.name = name;
-        this.average = average;
-        Miles = miles;
-        this.price = price;
+
+        String name;
+        int average;
+        int Miles;
+        double price;
+
+        public cars(String name, int average, int miles, double price) {
+            this.name = name;
+            this.average = average;
+            Miles = miles;
+            this.price = price;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getAverage() {
+            return average;
+        }
+
+        public void setAverage(int average) {
+            this.average = average;
+        }
+
+        public int getMiles() {
+            return Miles;
+        }
+
+        public void setMiles(int miles) {
+            Miles = miles;
+        }
+
+        public double getPrice() {
+            return price;
+        }
+
+        public void setPrice(double price) {
+            this.price = price;
+        }
+
+        @Override
+        public String toString() {
+            return
+                    "name='" + name + '\'' +
+                    ", average=" + average +
+                    ", Miles=" + Miles +
+                    ", price=" + price +
+                    '}';
+        }
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAverage() {
-        return average;
-    }
-
-    public void setAverage(int average) {
-        this.average = average;
-    }
-
-    public int getMiles() {
-        return Miles;
-    }
-
-    public void setMiles(int miles) {
-        Miles = miles;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    @Override
-    public String toString() {
-        return "cars{" +
-                "name='" + name + '\'' +
-                ", average=" + average +
-                ", Miles=" + Miles +
-                ", price=" + price +
-                '}';
-    }
-}
--- Collection/consumer.java
+++ Collection/consumer.java
@@ -1,4 +1,6 @@
-package Collection;
+package Basic;
+
+
 
 import java.util.HashMap;
 import java.util.Map;
@@ -6,19 +8,22 @@ import java.util.Scanner;
 
 import static java.time.chrono.JapaneseEra.values;
 
-public class consumer {
-    public static void main(String[] args) {
-        Map<cars,String> ob=new HashMap <cars,String>() ;
-        ob.put(new cars("suzaki",500,455,4550000.00),"Huv");
-        ob.put(new cars("HUNDA cITY",15154,1114,1454.4524),"sEDAN");
-        ob.put(new cars("mARUTI",15154,1114,1454.4524),"HACHBACK");
-        Scanner scanner=new Scanner(System.in);
-        String type=scanner.next();
-        for (Map.Entry<cars,String> obs:ob.entrySet());
-        {
-            if (type.equals(ob.values())){
-                System.out.println(ob.entrySet());
+    public class consumer {
+        public static void main(String[] args) {
+            Map<String,cars> ob=new HashMap <String,cars>() ;
+            ob.put("HUV",new cars("suzaki",500,455,4550000.00));
+            ob.put("SEDAN",new cars("HUNDA cITY",15154,1114,1454.4524));
+            ob.put("HATCHBACK",new cars("mARUTI",15154,1114,1454.4524));
+            Scanner scanner=new Scanner(System.in);
+            System.out.println("Enter The Car Type" );
+            String type=scanner.next();
+            for (Map.Entry<String,cars> obs:ob.entrySet())
+            {
+                if (obs.getKey().equals(type)){
+                    System.out.println(obs.getValue());
+
+                }
             }
         }
     }
-}
+
